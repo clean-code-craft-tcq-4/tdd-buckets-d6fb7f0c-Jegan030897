3d318c186deed51d94d081d528240826c0065912
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-04 18:46:04 +0530

    Update rangeFinder.h

--- rangeFinder.h
+++ rangeFinder.h
@@ -1,5 +1,13 @@
 #include <stdio.h>
   
+typedef struct 
+{
+	int startRange;
+	int endRange;
+	int rangeCounter;
+} rangeFinder_s;
+
+extern rangeFinder_s
 
 void sortGivenIndexPosition(int *Range, int noOfElements, int received_position);
 void sortRange(int *getRange, int no_Of_Elements);
