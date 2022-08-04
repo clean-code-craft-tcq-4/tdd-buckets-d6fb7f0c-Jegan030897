91ff27be3838d0b1c9ea745ae929d07aa3294176
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-04 10:41:31 +0530

    Update rangeTester.c

--- rangeTester.c
+++ rangeTester.c
@@ -15,7 +15,21 @@ void Test_Case0()
   }
 }
 
+void Test_Case1()
+{
+  int currentRange[4] = {10,1,4,6};
+  int sortedRange[4] = {10,1,4,6};
+  int length = sizeof(currentRange)/sizeof(currentRange[0]);
+  sortGivenIndexPosition(currentRange, length, 1);
+  
+  for(int index = 0; index < length; index++)
+  {
+    assert(currentRange[index] == sortedRange[index]);
+  }
+}
+
 int main()
 {
   Test_Case0();
+  Test_Case1();
 }
