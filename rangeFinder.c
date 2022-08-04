37d93bca7890d60e697f1213820757785e2f6825
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-04 09:54:30 +0530

    Update rangeFinder.c

--- rangeFinder.c
+++ rangeFinder.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-void sortRange(int *Range, int noOfElements)
-{
+void sortRange(int *Range, int noOfElements, int received_position) {
 	int temp;
-	for(int i = 0; i < noOfElements; i++)
-	{
-	    //for(int j = i+1; j < noOfElements; j++)
-	    //{
-		  if(Range[i] > Range[j])
-		  {
-			temp = Range[i];
-			Range[i] = Range[j];
-			Range[j] = temp;
-		  }
-	    //}
-	}
+	
+	for(int index = received_position+1 ; index < noOfElements; index++) {
+	if(Range[received_position] > Range[index]) {
+	temp = Range[received_position];
+     	Range[received_position] = Range[index];
+     	Range[index] = temp;
+   	}
+ 	}
+}
+
+void DetectRange(int *getRange, int no_Of_Elements) {
+  	for(int index = 0; index < no_Of_Elements; index++) {
+    	sortRange(getRange, no_Of_Elements, index); 
+  	}
 }
 
 
