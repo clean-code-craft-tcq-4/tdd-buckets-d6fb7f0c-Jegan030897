877aa8c219646a8d08386781b3ae2d9bb849210a
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-04 21:15:52 +0530

    Update rangeFinder.h

--- rangeFinder.h
+++ rangeFinder.h
@@ -11,7 +11,7 @@ extern rangeFinder_s rangeList[];
 
 void sortGivenIndexPosition(int *Range, int noOfElements, int received_position);
 void sortRange(int *getRange, int no_Of_Elements);
-//int get_noofRanges(int *getsortRange, int no_Of_Elements);
+int get_noofRanges(int *getsortRange, int no_Of_Elements);
 void getsortedRange(int *getsortRange, int no_Of_Elements);
 void findRange(int *receiveRange, int no_Of_Elements);
 void printRange();
