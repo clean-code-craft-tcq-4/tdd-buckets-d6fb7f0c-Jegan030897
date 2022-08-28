7abd8ec4e8475b344c5ceb8a04f4eb774e4e5ec4
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-28 14:33:17 +0530

    Update currentRangeFinder.h

--- currentRangeFinder.h
+++ currentRangeFinder.h
@@ -12,6 +12,6 @@ extern current_rangeFinder_s current_rangeList[];
 void sortGivenIndexPosition(int *Range, int noOfElements, int received_position);
 void sort_currentRangeList(int *get_currentRanges, int no_Of_availableReadings);
 int get_noofRanges(int *getsortRange, int no_Of_Elements);
-void get_currentRangeList(int *getsorted_currentRange, int no_Of_Elements);
+void get_currentRangesandCounter(int *getsorted_currentRange, int no_Of_Elements);
 int find_currentRangeList(int *received_currentRanges, int no_Of_Elements);
 void update_RangefinderStruct_Defaultvalue(int no_of_currentRanges);
