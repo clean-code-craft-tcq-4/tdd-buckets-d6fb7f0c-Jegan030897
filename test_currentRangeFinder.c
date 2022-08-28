8a8bda7dc25057b55b457e5216c3c8002c3d2dbd
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-28 13:57:35 +0530

    Update test_currentRangeFinder.c

--- test_currentRangeFinder.c
+++ test_currentRangeFinder.c
@@ -5,19 +5,22 @@
 /* sorting given current Ranges */
 void Test_Case0()
 {
-  int currentRange_1[4] = {10,1,4,6}, currentRange_2[10] = {10,1,4,6,2,5,3,8,9,6};
-  int sorted_currentRange_1[4] = {1,4,6,10}, sorted_currentRange_2[10] = {1,2,3,4,5,6,6,8,9,10};
+  int currentRange_1[4] = {10,1,4,6}, currentRange_2[10] = {10,1,4,6,2,5,3,8,9,6}, currentRange_3[5] = {10,10,10,10,10};
+  int sorted_currentRange_1[4] = {1,4,6,10}, sorted_currentRange_2[10] = {1,2,3,4,5,6,6,8,9,10}, sorted_currentRange_3[5] = {10,10,10,10,10};
   
   int length = sizeof(currentRange_1)/sizeof(currentRange_1[0]);
   int length1 = sizeof(currentRange_2)/sizeof(currentRange_2[0]);
+  int length2 = sizeof(currentRange_3)/sizeof(currentRange_3[0]);
   
   sort_currentRangeList(currentRange_1, length);
   sort_currentRangeList(currentRange_2, length1);
+  sort_currentRangeList(currentRange_3, length2);
   
   for(int index = 0; index < length; index++)
   {
     assert(currentRange_1[index] == sorted_currentRange_1[index]);
     assert(currentRange_2[index] == sorted_currentRange_2[index]);
+    assert(currentRange_3[index] == sorted_currentRange_3[index]);
   }
 }
 #if 0
