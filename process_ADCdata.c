02bf9cbc66de7ca7bd7ac45a7a041e50ec1990f4
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-30 18:19:25 +0530

    Update process_ADCdata.c

--- process_ADCdata.c
+++ process_ADCdata.c
@@ -1,11 +1,17 @@
+#include <math.h>
 #include "sensor_Config.h"
+
+int convertedAmpereValue[10];
 
 const mcalConfig* received_ADC_SensorConfig;
 
-int ADC_data_AssertCheck(int adcData, int adc_channelID)
+void update_sensorConfigObj()
 {
 	received_ADC_SensorConfig = get_sensorConfig(adc_channelID);
-	
+}
+
+int ADC_data_AssertCheck(int adcData, int adc_channelID)
+{
 	if(adcData > 0 && adcData <= received_ADC_SensorConfig->ADC_Resolution)
 	{
 		return IN_RANGE;
@@ -14,4 +20,9 @@ int ADC_data_AssertCheck(int adcData, int adc_channelID)
 	{
 		return OUTOFF_RANGE;
 	}
+}
+
+int convert_ADCCount_into_Amps(int adcData)
+{
+ 	return round((received_ADC_SensorConfig->sensor_AmpereRange * adcData) / (received_ADC_SensorConfig->ADC_Resolution));
 }
