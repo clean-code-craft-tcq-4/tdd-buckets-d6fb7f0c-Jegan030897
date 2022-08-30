48e05500df40ed0268024df4647438b0f94e37a0
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-30 18:23:22 +0530

    Update sensor_Config.h

--- sensor_Config.h
+++ sensor_Config.h
@@ -9,6 +9,7 @@ typedef struct
 	int sensor_AmpereRange;
 }mcalConfig;
 
-void update_sensorConfigObj();
+void update_sensorConfigObj(int adc_channelID);
 const mcalConfig* get_sensorConfig(int adc_channelID);
 int ADC_data_AssertCheck(int adcData, int adc_channelID);
+int convert_ADCCount_into_Amps(int adcData);
