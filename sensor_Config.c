5eecf28d8b7bcef59fd647bc012508b2f0f8d810
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-30 16:57:04 +0530

    Update sensor_Config.c

--- sensor_Config.c
+++ sensor_Config.c
@@ -2,7 +2,7 @@
 
 const mcalConfig ADC_SensorConfig[1] = {{4095, 10}};
 
-mcalConfig* get_sensorConfig(int adc_channelID)
+const mcalConfig* get_sensorConfig(int adc_channelID)
 {
   return &ADC_SensorConfig[adc_channelID];
 }
