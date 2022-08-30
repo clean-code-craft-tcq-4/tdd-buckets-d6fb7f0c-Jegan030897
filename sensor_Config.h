9cf94c430ec57cb9af99c2ccf8f25907e3af74dd
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-30 18:42:41 +0530

    Update sensor_Config.h

--- sensor_Config.h
+++ sensor_Config.h
@@ -13,6 +13,6 @@ typedef struct
 
 void update_sensorConfigObj(int adc_channelID);
 const mcalConfig* get_sensorConfig(int adc_channelID);
-int ADC_data_AssertCheck(int adcData, int adc_channelID);
+int ADC_data_AssertCheck(int adcData);
 int convert_ADCCount_into_Amps(int adcData);
 void GetandProcessSensor_ADC_data(int adcData, int adc_channelID);
