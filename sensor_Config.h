ed2443634e46078a70994c96d4bafb1e663f4fdc
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-08-30 16:57:02 +0530

    Update sensor_Config.h

--- sensor_Config.h
+++ sensor_Config.h
@@ -1,10 +1,13 @@
 #include<stdio.h>
 
+#define TRUE 1
+#define FALSE 0
+
 typedef struct
 {
 	int ADC_Resolution;
 	int sensor_Range;
 }mcalConfig;
 
-mcalConfig* get_sensorConfig(int adc_channelID);
+const mcalConfig* get_sensorConfig(int adc_channelID);
 int ADC_data_AssertCheck(int adcData, int adc_channelID);
